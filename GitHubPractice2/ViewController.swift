6ede703ce02658020ec16ff0ce00f8919c853049
Aiden <aoverton4508@stu.d214.org>
2021-12-10 10:15:40 -0600

    edited view controller

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet weak var tableView: UITableView!
+    var animal = ["Dog", "Dolphin", "Elephant"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animal.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mycell", for: indexPath)
+        cell.textLabel?.text = animal[indexPath.row]
+        
+        return cell
+    }
+    
 
 }
 
